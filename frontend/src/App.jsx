7444257bec0052e604376a201f36371a2de3c9de
Pedro Araújo <146120448+Pedroo-araujooo@users.noreply.github.com>
2026-08-13 14:26:48 -0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import GlobalStyle from "./styles/global.jsx";
+import styled from "styled-components";
+import Form from "./components/Form.jsx";
+import Grid from "./components/Grid.jsx";
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { useEffect } from "react";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+
+function App() {
+  const [users, setUsers] = useState([])
+  const [onEdit, setOnEdit] = useState(null)
+
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch(error) {
+      toast.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers])
+
+
+  return (
+   <>
+
+   <Container>
+    <Title>Usuários</Title>
+    <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers}/>
+    <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit}/>
+   </Container>
+   <ToastContainer autoClose={3000} position="bottom-left" />
+   <GlobalStyle />
+   </>
+  )
+}
+
+export default App
